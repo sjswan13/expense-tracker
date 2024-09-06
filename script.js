1abf20d95509fd5cdab8b30bfd416ee8ab05f23a
sjswan13 <sjswan13@gmail.com>
2024-09-06 15:35:05 -0600

    updating values js

--- script.js
+++ script.js
@@ -26,10 +26,32 @@ function addTransactionDOM(transaction) {
   item.classList.add(transaction.amount < 0 ? 'minus' : 'plus');
 
   item.innerHTML= `
-  ${transaction.text}<span>${sign}${Math.abs(transaction.amount)}</span> <button class='delete-btn'></button>
+  ${transaction.text}<span>${sign}${Math.abs(transaction.amount)}</span> <button class='delete-btn'>x</button>
   `;
 
   list.appendChild(item);
+}
+
+//Update Balance, income and expense
+function updateValues() {
+  const amounts = transactions.map(transaction => transaction.amount);
+
+  const total = amounts.reduce((acc, item) => (acc + item), 0).toFixed(2);
+
+  const income = amounts
+                    .filter(item => item > 0)
+                    .reduce((acc, item) => (acc += item), 0)
+                    .toFixed(2);
+  
+  const expense = (
+    amounts.filter(item => item < 0)
+      .reduce((acc, item) => 
+      (acc += item), 0) * -1).toFixed(2);
+
+  balance.innerText = `$${total}`;
+  money_plus.innerText = `$${income}`;
+  money_minus.innerText = `$${expense}`;
+  
 }
 
 //Init app
@@ -37,6 +59,7 @@ function init() {
   list.innerHTML = '';
 
   transactions.forEach(addTransactionDOM);
+  updateValues();
 }
 
 init();
